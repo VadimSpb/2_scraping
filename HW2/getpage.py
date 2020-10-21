4e4beaf495b74899154dd851198bf3e06475641c
VadimSPb <v.mazeyko@gmail.com>
2020-10-21 08:32:24 +0300

    1. Косметические улучшения HH_parcer 2. Запрос страницы выделен в отдельный файл

--- HW2/getpage.py
+++ HW2/getpage.py
@@ -1,9 +1,18 @@
 from bs4 import BeautifulSoup as BS
 import requests
 
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
+headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) \
+         AppleWebKit/537.36 (KHTML, like Gecko) \
+         Chrome/76.0.3809.100 Safari/537.36 OPR/63.0.3368.53'}
 
 def getpage(url, headers=headers, params=None):
 	responce = requests.get(url, headers=headers, params=params)
-	soup  = BS(responce.text, "html.parser")
+
+	# проверяем доступность страницы
+	if html.status_code is not requests.codes.ok:
+		print('Страница ' + html.url + ' не доступна')
+		break
+	# делаем объект bs
+	soup = BS(responce.text, 'lxml')
+
 	return soup
