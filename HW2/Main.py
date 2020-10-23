036a0dabb8980bb9c2b54397f38c97787e636398
VadimSPb <v.mazeyko@gmail.com>
2020-10-23 22:43:21 +0300

    наладка пейджератора на НН

--- HW2/Main.py
+++ HW2/Main.py
@@ -1,7 +1,21 @@
-
+import pandas as pd
+from hh_parcer import hh_parcer
+from sj_parcer import hh_parcer
 
 if __name__=='__main__':
     vacancy = input("Введите название вакансии:")
-    df = hh_parcer()
-    print(df.to_string())
-    print('На superjob.ru не обнаружено интересующих меня вакансий')
+    df_hh = hh_parcer(vacancy)
+    if len(df_hh) == 0:
+        print('На hh.ru не обнаружено вакансий')
+    else:
+        print(df.to_string())
+
+    df_sj = sj_parcer(vacancy)
+    if len(df_sj) == 0:
+        print('На superjob.ru не обнаружено вакансий')
+    else:
+        print(df.to_string())
+
+    df = pd.concat([df_hh, df_sj], ignore_index=True)
+    df.to_csv('vacancies.csv', index=False, header=True, encoding='utf-8')
+    print("итоговый файл vacancies.csv сохранён в каталоге проекта.")
