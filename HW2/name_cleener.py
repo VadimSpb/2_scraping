f41e2553c8d03a7c1fd28a7a77d31dcfbf0e7f0d
VadimSPb <v.mazeyko@gmail.com>
2020-10-23 19:06:12 +0300

    Пытаюсь наладить гит. Добавить парсер суперджоба

---
+++ HW2/name_cleener.py
@@ -0,0 +1,14 @@
+import re
+
+def hh_name_cleener(vacancy_name):
+    vacancy_name = re.sub('\+', "%2B", vacancy_name)
+    vacancy_name = re.sub('#', "%23", vacancy_name)
+    vacancy_name = re.sub(' +', "+", vacancy_name)
+    return vacancy_name
+
+def sj_name_cleener(vacancy_name):
+    vacancy_name = re.sub('\+',"%2B",vacancy_name);
+    vacancy_name = re.sub('#',"%23",vacancy_name);
+    vacancy_name = re.sub(' +',"%20",vacancy_name);
+    return vacancy_name
+
