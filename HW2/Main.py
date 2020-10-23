8246345af2ab46e5ebdb9fe370c6ffa0a3f7c394
VadimSPb <v.mazeyko@gmail.com>
2020-10-23 21:56:50 +0300

    налажен пейджератор в суперджоб.
    Внимание: не работает в пачарме, запускается из ноутбука!
    Внимание: Изменился тег для блока работодателей.

--- HW2/Main.py
+++ HW2/Main.py
@@ -1,6 +1,7 @@
 
 
 if __name__=='__main__':
+    vacancy = input("Введите название вакансии:")
     df = hh_parcer()
     print(df.to_string())
     print('На superjob.ru не обнаружено интересующих меня вакансий')
--- HW2/wellcome_file.py
+++ HW2/wellcome_file.py
@@ -1,4 +1,7 @@
 from  vac_name_cleener import vac_name_cleener
 from HH_parcer import HH_parcer
+from sj_parcer import sj_parcer
 
-vacancy = input("Введите название вакансии:")
+df = sj_parcer('c#')
+print(df)
+
